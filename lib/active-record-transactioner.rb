303c074fd72984aa153f077d1b67bdb230c130cc
Kasper Johansen <k@spernj.org>
2014-05-18 10:59:35 +0200

    Added max running threads and debugging. Close active record connections after usage to prevent no connection bugs.

--- lib/active-record-transactioner.rb
+++ lib/active-record-transactioner.rb
@@ -5,7 +5,9 @@ class ActiveRecordTransactioner
     :call_args => [],
     :call_method => :save!,
     :transaction_method => :transaction,
-    :transaction_size => 1000
+    :transaction_size => 1000,
+    :max_running_threads => 2,
+    :debug => false
   }
   
   ALLOWED_ARGS = DEFAULT_ARGS.keys
@@ -22,6 +24,7 @@ class ActiveRecordTransactioner
     @lock = Monitor.new
     @lock_threads = Monitor.new
     @lock_models = {}
+    @debug = @args[:debug]
     
     if block_given?
       begin
@@ -42,13 +45,15 @@ class ActiveRecordTransactioner
       @models[klass] = [] if !@models.key?(klass)
       @models[klass] << model
       @count += 1
-      flush if @count >= @args[:transaction_size]
     end
+    
+    flush if @count >= @args[:transaction_size]
   end
   
   #Flushes the specified method on all the queued models in a thread for each type of model.
   def flush
     threads = []
+    wait_for_threads
     
     @lock.synchronize do
       @models.each do |klass, val|
@@ -63,8 +68,10 @@ class ActiveRecordTransactioner
           thread = Thread.new do
             begin
               @lock_models[klass].synchronize do
+                debug "Opening new transaction by using '#{@args[:transaction_method]}'."
                 klass.__send__(@args[:transaction_method]) do
                   models.each do |model|
+                    # debug "Saving #{model.class.name}(#{model.id}) with method #{@args[:call_method]}"
                     model.__send__(@args[:call_method], *@args[:call_args])
                   end
                 end
@@ -74,7 +81,6 @@ class ActiveRecordTransactioner
               puts e.backtrace
               
               if e.is_a?(NoMethodError) and e.message.to_s.include?("`reverse' for nil:NilClass")
-                puts
                 puts "Warning: Known Rails reverse error when using transaction - retrying in 2 sec."
                 sleep 2
                 puts "Retrying"
@@ -84,7 +90,12 @@ class ActiveRecordTransactioner
               
               raise e
             ensure
+              debug "Removing thread #{Thread.current.__id__}"
               @threads.delete(Thread.current)
+              
+              @lock.synchronize do
+                ActiveRecord::Base.connection.close if ActiveRecord::Base.connection
+              end
             end
           end
         end
@@ -108,5 +119,30 @@ class ActiveRecordTransactioner
         thread.join
       end
     end
+  end
+  
+private
+  
+  def debug(str)
+    puts "{ActiveRecordTransactioner}: #{str}" if @debug
+  end
+  
+  def wait_for_threads
+    break_loop = false
+    while !break_loop
+      debug "Trying to lock..." if @debug
+      @lock.synchronize do
+        debug "Running threads: #{@threads.length} / #{@args[:max_running_threads]}"
+        if @threads.length < @args[:max_running_threads]
+          break_loop = true
+        else
+          debug "Waiting for threads #{@threads.length} / #{@args[:max_running_threads]}" if @debug
+        end
+      end
+      
+      sleep 0.2
+    end
+    
+    debug "Done waiting." if @debug
   end
 end
