478458927572ed3ba872aaabda9caa888aa47bbd
kaspernj <k@spernj.org>
2014-12-03 12:18:47 +0100

    Dont sleep if breaking loop. New hash syntax. Single blocks in one line.

--- lib/active-record-transactioner.rb
+++ lib/active-record-transactioner.rb
@@ -2,21 +2,19 @@ require "monitor"
 
 class ActiveRecordTransactioner
   DEFAULT_ARGS = {
-    :call_args => [],
-    :call_method => :save!,
-    :transaction_method => :transaction,
-    :transaction_size => 1000,
-    :max_running_threads => 2,
-    :debug => false
+    call_args: [],
+    call_method: :save!,
+    transaction_method: :transaction,
+    transaction_size: 1000,
+    max_running_threads: 2,
+    debug: false
   }
-  
+
   ALLOWED_ARGS = DEFAULT_ARGS.keys
-  
+
   def initialize(args = {})
-    args.each do |key, val|
-      raise "Invalid key: '#{key}'." unless ALLOWED_ARGS.include?(key)
-    end
-    
+    args.each { |key, val| raise "Invalid key: '#{key}'." unless ALLOWED_ARGS.include?(key) }
+
     @args = DEFAULT_ARGS.merge(args)
     @models = {}
     @threads = []
@@ -25,7 +23,7 @@ class ActiveRecordTransactioner
     @lock_threads = Monitor.new
     @lock_models = {}
     @debug = @args[:debug]
-    
+
     if block_given?
       begin
         yield self
@@ -35,35 +33,35 @@ class ActiveRecordTransactioner
       end
     end
   end
-  
+
   #Adds another model to the queue and calls 'flush' if it is over the limit.
   def queue(model)
     @lock.synchronize do
       klass = model.class
-      
-      @lock_models[klass] = Mutex.new if !@lock_models.key?(klass)
-      @models[klass] = [] if !@models.key?(klass)
+
+      @lock_models[klass] = Mutex.new unless @lock_models.key?(klass)
+      @models[klass] = [] unless @models.key?(klass)
       @models[klass] << model
       @count += 1
     end
-    
+
     flush if @count >= @args[:transaction_size]
   end
-  
+
   #Flushes the specified method on all the queued models in a thread for each type of model.
   def flush
     threads = []
     wait_for_threads
-    
+
     @lock.synchronize do
       @models.each do |klass, val|
         next if val.empty?
-        
+
         models = val
         @models[klass] = []
         @count -= models.length
         thread = nil
-        
+
         @lock_models[klass].synchronize do
           thread = Thread.new do
             begin
@@ -79,39 +77,37 @@ class ActiveRecordTransactioner
             rescue => e
               puts e.inspect
               puts e.backtrace
-              
-              if e.is_a?(NoMethodError) and e.message.to_s.include?("`reverse' for nil:NilClass")
+
+              if e.is_a?(NoMethodError) && e.message.to_s.include?("`reverse' for nil:NilClass")
                 puts "Warning: Known Rails reverse error when using transaction - retrying in 2 sec."
                 sleep 2
                 puts "Retrying"
                 puts
                 retry
               end
-              
+
               raise e
             ensure
-              debug "Removing thread #{Thread.current.__id__}"
+              debug "Removing thread #{Thread.current.__id__}" if @debug
+
               @threads.delete(Thread.current)
-              
-              @lock.synchronize do
-                ActiveRecord::Base.connection.close if ActiveRecord::Base.connection
-              end
+              @lock.synchronize { ActiveRecord::Base.connection.close if ActiveRecord::Base.connection }
             end
           end
         end
-        
+
         @lock_threads.synchronize do
           threads << thread
           @threads << thread
         end
       end
     end
-    
+
     return {
-      :threads => threads
+      threads: threads
     }
   end
-  
+
   #Waits for any remaining running threads.
   def join
     @lock_threads.synchronize do
@@ -120,13 +116,13 @@ class ActiveRecordTransactioner
       end
     end
   end
-  
+
 private
-  
+
   def debug(str)
     puts "{ActiveRecordTransactioner}: #{str}" if @debug
   end
-  
+
   def wait_for_threads
     break_loop = false
     while !break_loop
@@ -139,10 +135,10 @@ private
           debug "Waiting for threads #{@threads.length} / #{@args[:max_running_threads]}" if @debug
         end
       end
-      
-      sleep 0.2
+
+      sleep 0.2 unless break_loop
     end
-    
+
     debug "Done waiting." if @debug
   end
 end
