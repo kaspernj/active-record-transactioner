bf8e6b48b7ee27abf3a229a5fc5d5e8ea12bd02b
kaspernj <k@spernj.org>
2015-11-30 14:08:12 +0100

    Define Dummy module

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,6 +10,8 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 require "active-record-transactioner"
 
+module Dummy; end
+
 class Dummy::Application < Rails::Application
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
