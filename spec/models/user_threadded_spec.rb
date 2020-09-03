db38949179ba9ad92cb14913fbad28ad475b799a
kaspernj <kaspernj@gmail.com>
2020-09-03 19:38:07 +0200

    Fixed offences

--- spec/models/user_threadded_spec.rb
+++ spec/models/user_threadded_spec.rb
@@ -1,10 +1,8 @@
 require "spec_helper"
 
 describe User do
-  def transactioner
-    ActiveRecordTransactioner.new(transaction_size: 50, threadded: true) do |transactioner|
-      yield transactioner
-    end
+  def transactioner(&blk)
+    ActiveRecordTransactioner.new(transaction_size: 50, threadded: true, &blk)
   end
 
   it_behaves_like "basic user operations"
